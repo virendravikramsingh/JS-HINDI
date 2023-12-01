76c459b02a30ec7fbd7dcc7647a53aa95d52c953
virendravikramsingh <110225247+virendravikramsingh@users.noreply.github.com>
2023-12-01 09:41:38 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all code as newer version
+//alert(3+3) we are using nodejs not browser
+console.log(3+3)//code readability should be high
+console.log(sinu)
+
+//number= 2 to 2power53
+//bigint
+//string="abc"
+//bollean=true or false
+//null=standalone value
+//symbol
+//object
+console.log(typeof null) //object
+console.log(typeof unidentified)// unidentified
