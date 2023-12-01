5b998ad2458d26da195c150d39903339bd7c5e0a
virendravikramsingh <110225247+virendravikramsingh@users.noreply.github.com>
2023-12-01 10:47:00 +0000

    conversion

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let A="33ab"
+console.log(typeof A);
+let changeinnumber=Number(A)
+console.log(typeof changeinnumber);
+console.log(changeinnumber);
+//33=>33
+//33abc=>nan
+//true=>1,false=>0
+
+let b=1
+let changeinboolean =Boolean(b)
+console.log(changeinboolean);
+//1=>true ,0=>false
+//emptystring=>false,string=true
+    
+        //operations
+        // console.log( 1 + 2);
+        // console.log( 1 + "2");
+        // console.log( "1"+ 2);
+        // console.log( "1"+ 1 + 2);
+        // console.log( 1 + 1 + "2")
+        // console.log(1+"1"+2);
+
+        
