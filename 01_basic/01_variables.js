07535298a16587c7bb9b43e9294724c24f9b2298
virendravikramsingh <110225247+virendravikramsingh@users.noreply.github.com>
2023-12-01 09:41:09 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountno=1234;
+let accountemail=("sinu@gmail.com")
+var accountpassword=("abcdef")
+accountcity="benglore"
+/*
+        don't use var because issues
+        in block and functional scope.
+*/
+
+accountemail=("hdfcdks")
+accountpassword=("bnmnzxv")
+accountcity=("mumbai")
+console.table([accountemail,accountpassword,accountno,accountcity])
+
