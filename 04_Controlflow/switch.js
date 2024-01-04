9a7d0a089d663d18d856cbd7e8edd2050c401f76
virendravikramsingh <110225247+virendravikramsingh@users.noreply.github.com>
2024-01-04 07:07:28 +0000

    okk

---
+++ 04_Controlflow/switch.js
@@ -0,0 +1,27 @@
+const value=4
+
+switch (value) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 1:
+        console.log("january");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+
+    default:
+        break;
+}
